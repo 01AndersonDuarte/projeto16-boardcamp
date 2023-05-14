6f6e9ad628ded1f6759c6f3e960fd38020124271
Anderson Duarte <andersonduarte997@gmail.com>
2023-05-14 19:41:36 -0300

    feat: CRUD de alugueis finalizado

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -12,7 +12,7 @@ export async function postRental(req, res) {
         if (!gameFindId.rowCount) return res.status(400).send("Produto não encontrado.");
 
         const rentedGame = await db.query(`SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL;`, [gameId]);
-        if(rentedGame.rowCount===gameFindId.rows[0].stockTotal) return res.status(400).send("Não disponível.");
+        if (rentedGame.rowCount === gameFindId.rows[0].stockTotal) return res.status(400).send("Não disponível.");
 
         const listInsert = [customerId, gameId, dayjs().format("YYYY-MM-DD"), daysRented, null, daysRented * gameFindId.rows[0].pricePerDay, null]
 
@@ -23,6 +23,58 @@ export async function postRental(req, res) {
             VALUES ($1, $2, $3, $4, $5, $6, $7);`, listInsert
         );
         res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function getRentals(req, res) {
+    try {
+        const rentals = await db.query(`
+        SELECT rentals.*, customers.name AS "customer", games.name AS "game"
+        FROM rentals JOIN customers
+        ON rentals."customerId" = customers.id
+        JOIN games
+        ON rentals."gameId" = games.id;`);
+        res.send(rentals.rows.map(r => ({ ...r, customer: { id: r.customerId, name: r.customer }, game: { id: r.gameId, name: r.game } })));
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function finishRent(req, res) {
+    const { id } = req.params;
+
+    const findRent = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id]);
+    if (!findRent.rowCount) return res.sendStatus(404);
+    if (findRent.rows[0].returnDate) return res.sendStatus(400);
+
+    const pricePerDay = findRent.rows[0].originalPrice / findRent.rows[0].daysRented;
+    const date1 = findRent.rows[0].rentDate;
+    const date2 = dayjs(dayjs().format("YYYY-MM-DD"));
+    const diffDays = Math.floor(date2.diff(date1) / 86400000);
+
+    try {
+        await db.query(`
+            UPDATE rentals SET "returnDate" = $1, "delayFee" = $2
+            WHERE id = $3;`,
+            [date2, diffDays * pricePerDay, id]
+        );
+        res.send("Produto devolvido.");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function deleteRent(req, res) {
+    const { id } = req.params;
+    try {
+        const findRent = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id]);
+        if (!findRent.rowCount) return res.sendStatus(404);
+        if (!findRent.rows[0].returnDate) return res.sendStatus(400);
+
+        await db.query(`DELETE FROM rentals WHERE id = $1;`, [id]);
+        res.send("Registro deletado com sucesso.");
     } catch (error) {
         res.status(500).send(error.message);
     }
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
-import { postRental } from "../controllers/rentals.controller.js"
+import { postRental, getRentals, finishRent, deleteRent } from "../controllers/rentals.controller.js"
 import { addRental } from "../schemas/addRental.schema.js";
 import schemaValidation from "../middlewares/schemaValidation.middleware.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals", schemaValidation(addRental), postRental);
+rentalsRouter.get("/rentals", getRentals);
+rentalsRouter.post("/rentals/:id/return", finishRent);
+rentalsRouter.delete("/rentals/:id", deleteRent);
 
 export default rentalsRouter;
