824d255583f169d40609c9d22665e9f6f783334e
Anderson Duarte <andersonduarte997@gmail.com>
2023-05-14 20:03:15 -0300

    Atualização de status code

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -11,7 +11,7 @@ export async function postGames(req, res) {
             INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
             VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
 
-        res.send("Item adicionado com sucesso");
+        res.status(201).send("Item adicionado com sucesso");
     } catch (error) {
         res.status(500).send(error.message);
     }
